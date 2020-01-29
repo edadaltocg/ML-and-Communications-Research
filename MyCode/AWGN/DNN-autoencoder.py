80ef439d292686f81f7a474123a6fa8198c7fc6e
Eduardo <edadaltocg@gmail.com>
2020-01-29 15:08:51 +0100

    Delete codes

--- MyCode/AWGN/DNN-autoencoder.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep 15 14:02:11 2019
-
-@author: user
-"""
-
--- MyCode/BSC/test.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 29 17:57:38 2019
-
-@author: user
-"""
-print('SUP')
